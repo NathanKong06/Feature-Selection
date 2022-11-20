ae4a6acf92e93416d9798e4c7cb23d069eeb9267
NateGaNe <81611522+NathanKong06@users.noreply.github.com>
2022-11-19 21:27:26 -0800

    Update FeatureSelection.cpp

--- FeatureSelection.cpp
+++ FeatureSelection.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 using namespace std;
 
-int main(){
+double crossValidation(vector<int> currentSet, int featureToAdd){\
+    double accuracy;
+    ifstream data ("CS170_Small_Data__43.txt");
+    data.close();
+    accuracy = rand();
+    return accuracy;
+}
 
+int main(){
+    string line;
+    ifstream file ("CS170_Small_Data__43.txt");
+    
+    while (getline(file,line)){
+        cout << line << endl;
+    }
+
+    file.close();
+
+    return 0;
 }
