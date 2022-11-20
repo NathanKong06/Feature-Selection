7480e4796f187e27a348daf9f07df40554f49d23
NateGaNe <81611522+NathanKong06@users.noreply.github.com>
2022-11-19 23:06:57 -0800

    Update FeatureSelection.cpp

--- FeatureSelection.cpp
+++ FeatureSelection.cpp
@@ -2,9 +2,10 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <algorithm>
 using namespace std;
 
-double crossValidation(vector<int> currentSet, int featureToAdd){\
+double crossValidation(vector<int> currentSet, int featureToAdd){
     double accuracy;
     ifstream data ("CS170_SuperSmall_Data__43.txt");
     data.close();
@@ -15,26 +16,42 @@ double crossValidation(vector<int> currentSet, int featureToAdd){\
 void feature_search_demo(){
     unsigned int rowCounter = 0;
     unsigned int columnCounter = 0;
+    unsigned int featureToAddAtCurrentLevel = -1;
+    double bestAccuracy = 0.0;
+    double accuracy = 0.0;
+
     string line;
     string featureValue;
     string classValue;
+
+    vector<int> currentSetOfFeatures;
+
     ifstream file ("CS170_SuperSmall_Data__43.txt");
     
-    while (getline(file,line)) {
-        rowCounter++;
+    while (getline(file,line)) { //Every Row in File
+        rowCounter++; //Current Row Number
         cout << "On the " << rowCounter << "th level of the search tree" << endl;
+        featureToAddAtCurrentLevel = -1;
+        bestAccuracy = 0;
         stringstream ss(line);
-        ss >> classValue;
-        while (ss >> featureValue){
+        ss >> classValue; //Take in Class Value
+
+        while (ss >> featureValue){ //Every Value (Column) in Row Except Class Value
             columnCounter++;
-            cout << "--Considering adding the " << columnCounter << " feature" << endl;
+            if (find(currentSetOfFeatures.begin(),currentSetOfFeatures.end(),columnCounter) == currentSetOfFeatures.end()){
+                cout << "--Considering adding the " << columnCounter << " feature" << endl;
+                accuracy = crossValidation(currentSetOfFeatures,columnCounter + 1);
+
+                if (accuracy > bestAccuracy){
+                    bestAccuracy  = accuracy;
+                    featureToAddAtCurrentLevel = columnCounter;
+                }
+            }
         }
         columnCounter = 0;
+        currentSetOfFeatures.push_back(featureToAddAtCurrentLevel);
+        cout << "On level: " << rowCounter << " I added feature " << featureToAddAtCurrentLevel << " to current set" << endl;
     }
-
-    // while (ss >> featureValue){
-    //     cout << stod(featureValue) << " " ;
-    // }
 
     file.close();
 }
