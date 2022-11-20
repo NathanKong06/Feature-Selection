8adf653a3cab8578271f1feb774819cf932db0c2
NateGaNe <81611522+NathanKong06@users.noreply.github.com>
2022-11-19 22:42:33 -0800

    Add Test Data Set, Additional Code

--- FeatureSelection.cpp
+++ FeatureSelection.cpp
@@ -1,25 +1,38 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <sstream>
 using namespace std;
 
 double crossValidation(vector<int> currentSet, int featureToAdd){\
     double accuracy;
-    ifstream data ("CS170_Small_Data__43.txt");
+    ifstream data ("CS170_SuperSmall_Data__43.txt");
     data.close();
     accuracy = rand();
     return accuracy;
 }
 
-int main(){
+void feature_search_demo(){
+    unsigned int counter = 0;
     string line;
-    ifstream file ("CS170_Small_Data__43.txt");
+    string featureValue;
+    ifstream file ("CS170_SuperSmall_Data__43.txt");
     
-    while (getline(file,line)){
-        cout << line << endl;
+    while (getline(file,line)) {
+        counter++;
+        cout << "On the " << counter << "th level of the search tree" << endl;
     }
 
+    // while (ss >> featureValue){
+    //     cout << stod(featureValue) << " " ;
+    // }
+
     file.close();
+}
+
+int main(){
+
+    feature_search_demo();
 
     return 0;
 }
